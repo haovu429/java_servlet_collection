0db11aa8a8de7c0d6f815d3c2342912cf9fa887a
HaoVu <caihoncuagiamnguc@gmail.com>
2023-06-30 13:49:00 +0700

    add readme.md file

--- universal-store/target/ShopPhone-0.0.1-SNAPSHOT/template/web/js/cataloghome.js
+++
@@ -1,5 +0,0 @@
-function handleCatalog() {
-    const catalog = document.querySelector(".catalog-group");
-    catalog.classList.add("show-catalog");
-}
-handleCatalog();
--- universal-store/target/ShopPhone-0.0.1-SNAPSHOT/template/web/js/product.js
+++
@@ -1,3 +0,0 @@
-// Căn chỉnh homepageImage trong trang product.htm
-const homePage = document.querySelector(".homepage");
-homePage.style = "width : 100%";
